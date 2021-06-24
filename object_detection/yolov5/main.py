9c46e6ce9350ea4c6d4acccc2ae54b6efcd730d6
Aaron Grainer <aaronlimfz@gmail.com>
2021-06-24 17:09:07 +0800

    Reformated yolov5 detection to class method

--- object_detection/yolov5/main.py
+++ object_detection/yolov5/main.py
@@ -8,31 +8,35 @@ import cv2
 import pandas as pd
 
 
+class YoloDetector:
+    def __init__(self):
+        self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+
+    def _visualize(self, img, predictions):
+        v = Visualizer(img, scale=1.5)
+
+        for _, pred in predictions.iterrows():
+            box_coord = (pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'])
+            v.draw_box(box_coord, edge_color='moccasin')
+        out = v.get_output()
+        return out.get_image()[:, :, ::-1]
+
+    def detect_img(self, img):
+        results = self.model(img)
+        # classes = results.names
+        predictions = results.pandas().xyxy[0]
+        img_pred = self._visualize(img, predictions)
+
+        cv2.imshow('Predictions', img_pred)
+        cv2.waitKey()
+
+
 def main():
-    model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
-
     img_path = 'test_asset/solidWhiteCurve.jpg'
-
     img = cv2.imread(img_path)[:, :, ::-1]
 
-    results = model(img)
-    classes = results.names
-    predictions = results.pandas().xyxy[0]
-
-    MetadataCatalog.get(f"yolov5").set(thing_classes=classes)
-    yolov5_metadata = MetadataCatalog.get("yolov5")
-
-    v = Visualizer(img,
-                   metadata=yolov5_metadata,
-                   scale=0.5
-    )
-
-    for _, pred in predictions.iterrows():
-        box_coord = (pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'])
-        v.draw_box(box_coord, edge_color='moccasin')
-    out = v.get_output()
-    cv2.imshow('Pred', out.get_image()[:, :, ::-1])
-    cv2.waitKey()
+    yolo_detector = YoloDetector()
+    yolo_detector.detect_img(img)
 
 
 if __name__ == '__main__':
