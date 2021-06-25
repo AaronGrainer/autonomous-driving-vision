8d4fd1f006eb74d63ac097bdc393e1fc3faef3d9
Aaron Grainer <aaronlimfz@gmail.com>
2021-06-25 15:37:02 +0800

    Added sort with yolov5

--- object_detection/yolov5/main.py
+++ object_detection/yolov5/main.py
@@ -9,19 +9,45 @@ import fire
 import matplotlib
 import matplotlib.pyplot as plt
 
+from object_detection.utils.sort import Sort
+
 
 class YoloDetector:
     def __init__(self):
+        self.max_age = 1
+        self.min_hits = 3
+        self.iou_threshold = 0.3
+
         self.model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+        self.mot_tracker = Sort(
+            max_age=self.max_age,
+            min_hits=self.min_hits,
+            iou_threshold=self.iou_threshold
+        )
         matplotlib.use('TkAgg')
 
-    def _visualize(self, img, predictions):
+    def _update_tracker(self, preds):
+        detections = [[pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'], pred['confidence']] 
+                      for _, pred in preds.iterrows()]
+        print('detections: ', detections, len(detections))
+        trackers = self.mot_tracker.update(detections)
+        print('trackers: ', trackers, len(trackers))
+
+        for track in trackers[:-1]:
+            preds.loc[(preds['xmin'] == track[0]) & (preds['ymin'] == track[1]) &
+                      (preds['xmax'] == track[2]) & (preds['ymax'] == track[3]), 'track_id'] = track[4]
+        print('preds: ', preds)
+        preds.dropna(subset=['track_id'], inplace=True)
+        print('preds: ', preds)
+        return preds
+
+    def _visualize(self, img, preds):
         v = Visualizer(img, scale=1)
 
-        for _, pred in predictions.iterrows():
+        for _, pred in preds.iterrows():
             box_coord = (pred['xmin'], pred['ymin'], pred['xmax'], pred['ymax'])
             v.draw_box(box_coord, edge_color='moccasin')
-            v.draw_text(pred['name'], (pred['xmin'], pred['ymin']), font_size=8,
+            v.draw_text(f"{pred['name']} {int(pred['track_id'])}", (pred['xmin'], pred['ymin']), font_size=8,
                         color='moccasin', horizontal_alignment='left')
         out = v.get_output()
         return out.get_image()[:, :, ::-1]
@@ -30,12 +56,13 @@ class YoloDetector:
         results = self.model(img)
         # classes = results.names
         # print('classes: ', classes)
-        predictions = results.pandas().xyxy[0]
-        return self._visualize(img, predictions)
+        preds = results.pandas().xyxy[0]
+        preds = self._update_tracker(preds)
+        return self._visualize(img, preds)
 
     def detect_img(self, img):
         img_pred = self._detect(img)
-        cv2.imshow('Predictions', img_pred)
+        cv2.imshow('preds', img_pred)
         cv2.waitKey()
 
     def detect_video(self):
